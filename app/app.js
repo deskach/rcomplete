17114d250e69f5c27fd02dcd0a59fb206f17c01d
deskach <dennis.kachanau@rexsoftware.com.au>
2016-12-29 16:18:05 +1000

    Split Greeter into 2 sub-components

--- app/app.js
+++ app/app.js
@@ -1,40 +1,73 @@
 /**
  * Created by deskach on 29/12/16.
  */
-var Greeter = React.createClass({
+var Header = React.createClass({
   getDefaultProps: function () {
     return {
       message: 'This is from the component.',
     }
   },
 
-  getInitialState: function () {
-    return {
-      name: 'React'
-    }
-  },
+  render: function () {
+    const style = {
+      border: '1px solid green',
+      marginBottom: 10
+    };
 
+    return (
+        <div style={style}>
+          <h1>Hello {this.props.name}!</h1>
+          <p>{this.props.message}</p>
+        </div>
+    );
+  }
+});
+
+var NameInput = React.createClass({
   onFormSubmit: function (e) {
     e.preventDefault();
 
     const name = this.refs.name.value;
 
-    if(name) {
+    if (name) {
       this.refs.name.value = "";
-      this.setState({name});
+      this.props.onFormSubmit(name);
+    }
+  },
+
+  render: function () {
+    const style = {
+      border: '1px solid blue',
+      padding: 5,
+    };
+
+    return (
+        <div style={style}>
+          <form onSubmit={this.onFormSubmit}>
+            <input type="text" ref="name"/>
+            <button>Set Name</button>
+          </form>
+        </div>
+    );
+  }
+});
+
+var Greeter = React.createClass({
+  getInitialState: function () {
+    return {name: 'React'};
+  },
+
+  onFormSubmit: function (name) {
+    if (name) {
+      this.setState({ name });
     }
   },
 
   render: function () {
     return (
         <div>
-          <h1>Hello {this.state.name}!</h1>
-          <p>{this.props.message}</p>
-
-          <form onSubmit={this.onFormSubmit} >
-            <input type="text" ref="name"/>
-            <button>Set Name</button>
-          </form>
+          <Header name={this.state.name}/>
+          <NameInput onFormSubmit={this.onFormSubmit}/>
         </div>
     );
   }
